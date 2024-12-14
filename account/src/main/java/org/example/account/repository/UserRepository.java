fb61e440ec16235d9059773b7d3fd05ef9873a1c
Shin Hee Won <shinheewon0107@gmail.com>
2024-12-14 21:46:00 +0900

    Delete account directory

--- account/src/main/java/org/example/account/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.account.repository;
-
-import org.example.account.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-
-    Boolean existsByUsername(String username);
-}
