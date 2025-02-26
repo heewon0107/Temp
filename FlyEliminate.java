f0784dd5a76397e71b6c6e30f855c46792011b54
Heewon <shinheewon0107@gmail.com>
2025-02-26 16:00:22 +0900

    SCRUM-1 :sparkles: feat: 지라 연동 테스트

--- FlyEliminate.java
+++ FlyEliminate.java
@@ -8,20 +8,30 @@ import java.util.StringTokenizer;
 public class FlyEliminate  {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= testCase; i++) {
-            int N,range;
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            range = Integer.parseInt(st.nextToken());
-            String[][] flyArray = new String[N][N];
-            for (int j = 0; j < N; j++) {
-                String line = br.readLine();
-                flyArray[j] = line.split(" ");
-            }
-            
-
+        String line = br.readLine();
+        int[] counter = new int[123];
+        for (int i = 0; i < line.length(); i++) {
+            counter[line.charAt(i)]++;
         }
+        char maxChar = '';
+        int maxCount = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < counter.length; i++) {
+            // 최빈 값 == 맥스 인덱스 변경
+            if (counter[i] > maxCount) {
+                maxCount = counter[i];
+                maxIndex = i;
+            }
+            // 동일 값이면 ?로 변경
+            else if (counter[i] == maxCount && counter[i] > 0) {
+                maxChar = '?';
+            }
+        }
+        if (maxChar != '?') {
+            maxChar = (char) maxIndex;
+        }
+        System.out.println(maxChar);
+
 
     }
 }
